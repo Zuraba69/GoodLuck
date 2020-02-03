e4553a27877babbf7b6bb7063f13a3341489ef97
Zurikela <zurabi.kobakhidze.1@btu.edu.ge>
2020-02-03 15:11:07 +0400

    boom_tic_tac_toe_done

--- app/src/main/java/com/example/futureishere/TicTacToeActivity.kt
+++ app/src/main/java/com/example/futureishere/TicTacToeActivity.kt
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.core.graphics.toColorInt
 import kotlinx.android.synthetic.main.activity_tic_tac_toe.*
 
 class TicTacToeActivity : AppCompatActivity() {
     private var Player = true
+    var Xscore:Int = 0
+    var Oscore:Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +28,9 @@ class TicTacToeActivity : AppCompatActivity() {
 
     }
     private fun init(){
+        RestartButton.setOnClickListener  {
+            reset()
+        }
         button00.setOnClickListener {
             CheckPlayer(button00)
         }
@@ -75,6 +81,198 @@ class TicTacToeActivity : AppCompatActivity() {
             button.text = "O"
             Player = true
         }
+        if (button00.text.isNotEmpty() && button00.text == button01.text && button00.text == button02.text){
+            Toast.makeText(this,"Winner is ${button00.text}",Toast.LENGTH_SHORT).show()
+            if (button00.text == "X"){
+                Xscore ++
+                XscoreView.text = "" + Xscore
+            }else{
+                Oscore ++
+                OscoreView.text = "" + Oscore
+            }
+            DisableClicks()
+        }else if (button00.text.isNotEmpty() && button00.text == button11.text && button00.text == button22.text) {
+            Toast.makeText(this, "Winner is ${button00.text}", Toast.LENGTH_SHORT).show()
+            if (button00.text == "X"){
+                Xscore ++
+                XscoreView.text = "" + Xscore
+            }else{
+                Oscore ++
+                OscoreView.text = "" + Oscore
+            }
+            DisableClicks()
+        }else if (button00.text.isNotEmpty() && button00.text == button10.text && button00.text == button20.text) {
+            Toast.makeText(this, "Winner is ${button00.text}", Toast.LENGTH_SHORT).show()
+            if (button00.text == "X"){
+                Xscore ++
+                XscoreView.text = "" + Xscore
+            }else{
+                Oscore ++
+                OscoreView.text = "" + Oscore
+            }
+            DisableClicks()
+        }else if (button01.text.isNotEmpty() && button01.text == button11.text && button01.text == button21.text) {
+            Toast.makeText(this, "Winner is ${button01.text}", Toast.LENGTH_SHORT).show()
+            if (button01.text == "X"){
+                Xscore ++
+                XscoreView.text = "" + Xscore
+            }else{
+                Oscore ++
+                OscoreView.text = "" + Oscore
+            }
+            DisableClicks()
+        }else if (button02.text.isNotEmpty() && button02.text == button11.text && button02.text == button20.text) {
+            Toast.makeText(this, "Winner is ${button02.text}", Toast.LENGTH_SHORT).show()
+            if (button02.text == "X"){
+                Xscore ++
+                XscoreView.text = "" + Xscore
+            }else{
+                Oscore ++
+                OscoreView.text = "" + Oscore
+            }
+            DisableClicks()
+        }else if (button02.text.isNotEmpty() && button02.text == button01.text && button02.text == button00.text) {
+            Toast.makeText(this, "Winner is ${button02.text}", Toast.LENGTH_SHORT).show()
+            if (button02.text == "X"){
+                Xscore ++
+                XscoreView.text = "" + Xscore
+            }else{
+                Oscore ++
+                OscoreView.text = "" + Oscore
+            }
+            DisableClicks()
+        }else if (button02.text.isNotEmpty() && button02.text == button12.text && button02.text == button22.text) {
+            Toast.makeText(this, "Winner is ${button02.text}", Toast.LENGTH_SHORT).show()
+            if (button02.text == "X"){
+                Xscore ++
+                XscoreView.text = "" + Xscore
+            }else{
+                Oscore ++
+                OscoreView.text = "" + Oscore
+            }
+            DisableClicks()
+        }else if (button10.text.isNotEmpty() && button10.text == button11.text && button10.text == button12.text) {
+            Toast.makeText(this, "Winner is ${button10.text}", Toast.LENGTH_SHORT).show()
+            if (button10.text == "X"){
+                Xscore ++
+                XscoreView.text = "" + Xscore
+            }else{
+                Oscore ++
+                OscoreView.text = "" + Oscore
+            }
+            DisableClicks()
+        }else if (button12.text.isNotEmpty() && button12.text == button11.text && button12.text == button10.text) {
+            Toast.makeText(this, "Winner is ${button12.text}", Toast.LENGTH_SHORT).show()
+            if (button12.text == "X"){
+                Xscore ++
+                XscoreView.text = "" + Xscore
+            }else{
+                Oscore ++
+                OscoreView.text = "" + Oscore
+            }
+            DisableClicks()
+        }else if (button20.text.isNotEmpty() && button20.text == button10.text && button20.text == button00.text) {
+            Toast.makeText(this, "Winner is ${button20.text}", Toast.LENGTH_SHORT).show()
+            if (button20.text == "X"){
+                Xscore ++
+                XscoreView.text = "" + Xscore
+            }else{
+                Oscore ++
+                OscoreView.text = "" + Oscore
+            }
+            DisableClicks()
+        }else if (button20.text.isNotEmpty() && button20.text == button11.text && button20.text == button02.text) {
+            Toast.makeText(this, "Winner is ${button20.text}", Toast.LENGTH_SHORT).show()
+            if (button20.text == "X"){
+                Xscore ++
+                XscoreView.text = "" + Xscore
+            }else{
+                Oscore ++
+                OscoreView.text = "" + Oscore
+            }
+            DisableClicks()
+        }else if (button20.text.isNotEmpty() && button20.text == button21.text && button20.text == button22.text) {
+            Toast.makeText(this, "Winner is ${button20.text}", Toast.LENGTH_SHORT).show()
+            if (button20.text == "X"){
+                Xscore ++
+                XscoreView.text = "" + Xscore
+            }else{
+                Oscore ++
+                OscoreView.text = "" + Oscore
+            }
+            DisableClicks()
+        }else if (button21.text.isNotEmpty() && button21.text == button11.text && button21.text == button01.text) {
+            Toast.makeText(this, "Winner is ${button21.text}", Toast.LENGTH_SHORT).show()
+            if (button21.text == "X"){
+                Xscore ++
+                XscoreView.text = "" + Xscore
+            }else{
+                Oscore ++
+                OscoreView.text = "" + Oscore
+            }
+            DisableClicks()
+        }else if (button22.text.isNotEmpty() && button22.text == button21.text && button22.text == button20.text) {
+            Toast.makeText(this, "Winner is ${button22.text}", Toast.LENGTH_SHORT).show()
+            if (button22.text == "X"){
+                Xscore ++
+                XscoreView.text = "" + Xscore
+            }else{
+                Oscore ++
+                OscoreView.text = "" + Oscore
+            }
+            DisableClicks()
+        }else if (button22.text.isNotEmpty() && button22.text == button11.text && button22.text == button00.text) {
+            Toast.makeText(this, "Winner is ${button22.text}", Toast.LENGTH_SHORT).show()
+            if (button22.text == "X"){
+                Xscore ++
+                XscoreView.text = "" + Xscore
+            }else{
+                Oscore ++
+                OscoreView.text = "" + Oscore
+            }
+            DisableClicks()
+        }else if (button22.text.isNotEmpty() && button22.text == button12.text && button22.text == button02.text) {
+            Toast.makeText(this, "Winner is ${button22.text}", Toast.LENGTH_SHORT).show()
+            if (button22.text == "X"){
+                Xscore ++
+                XscoreView.text = "" + Xscore
+            }else{
+                Oscore ++
+                OscoreView.text = "" + Oscore
+            }
+            DisableClicks()
+        }
     }
+    private fun reset(){
+        button00.isClickable = true
+        button01.isClickable = true
+        button02.isClickable = true
+        button10.isClickable = true
+        button11.isClickable = true
+        button12.isClickable = true
+        button20.isClickable = true
+        button21.isClickable = true
+        button22.isClickable = true
 
+        button00.text = ""
+        button01.text = ""
+        button02.text = ""
+        button10.text = ""
+        button11.text = ""
+        button12.text = ""
+        button20.text = ""
+        button21.text = ""
+        button22.text = ""
+    }
+    private fun DisableClicks(){
+        button00.isClickable = false
+        button01.isClickable = false
+        button02.isClickable = false
+        button10.isClickable = false
+        button11.isClickable = false
+        button12.isClickable = false
+        button20.isClickable = false
+        button21.isClickable = false
+        button22.isClickable = false
+    }
 }
