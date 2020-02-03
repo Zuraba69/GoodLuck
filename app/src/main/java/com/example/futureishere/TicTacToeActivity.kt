e2d78a43249547ab8a17f46a4c2d014a837ff3b8
Zurikela <zurabi.kobakhidze.1@btu.edu.ge>
2020-02-03 16:38:21 +0400

    boom_tic_tac_toe_done_for_real_i_swear

--- app/src/main/java/com/example/futureishere/TicTacToeActivity.kt
+++ app/src/main/java/com/example/futureishere/TicTacToeActivity.kt
@@ -245,6 +245,8 @@ class TicTacToeActivity : AppCompatActivity() {
                 OscoreView.text = "" + Oscore
             }
             DisableClicks()
+        }else if (button00.text.isNotEmpty() && button01.text.isNotEmpty() && button02.text.isNotEmpty() && button10.text.isNotEmpty() && button11.text.isNotEmpty() && button12.text.isNotEmpty() && button20.text.isNotEmpty() && button21.text.isNotEmpty() && button22.text.isNotEmpty()) {
+            Toast.makeText(this, "Draw", Toast.LENGTH_SHORT).show()
         }
     }
     private fun reset(){
