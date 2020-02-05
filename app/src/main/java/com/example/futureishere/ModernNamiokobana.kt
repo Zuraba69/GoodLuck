0c436c0da68dbe0cb7e68e97a409bb4c0845ca38
Zurikela <zurabi.kobakhidze.1@btu.edu.ge>
2020-02-05 16:01:33 +0400

    boom_modern_namiokobana_game_done

--- app/src/main/java/com/example/futureishere/ModernNamiokobana.kt
+++ app/src/main/java/com/example/futureishere/ModernNamiokobana.kt
@@ -9,11 +9,13 @@ import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_modern_namiokobana.*
 import kotlinx.android.synthetic.main.activity_never_ever.*
 import java.util.*
+import kotlin.concurrent.timer
 
 class ModernNamiokobana : AppCompatActivity() {
     val words = arrayOf<String>("Chicken","Wall","Weed","Minecraft")
     var blue_score_kotlin:Int = 0
     var red_score_kotlin:Int = 0
+    var round_counter:Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +50,7 @@ class ModernNamiokobana : AppCompatActivity() {
             blue_score_kotlin ++
             blue_score.text = "" + blue_score_kotlin
             blue_score_in_board.text = "" + blue_score_kotlin
+            blue_score_in_board_2.text = "" + blue_score_kotlin
         }
         blue_wrong_answer.setOnClickListener{
             val r = Random()
@@ -56,6 +59,7 @@ class ModernNamiokobana : AppCompatActivity() {
             blue_score_kotlin --
             blue_score.text = "" + blue_score_kotlin
             blue_score_in_board.text = "" + blue_score_kotlin
+            blue_score_in_board_2.text = "" + blue_score_kotlin
         }
         next_round.setOnClickListener {
             score_bar.isVisible = false
@@ -70,6 +74,7 @@ class ModernNamiokobana : AppCompatActivity() {
             red_score_kotlin ++
             red_score.text = "" + red_score_kotlin
             red_score_in_board.text = "" + red_score_kotlin
+            red_score_in_board_2.text = "" + red_score_kotlin
         }
         red_wrong_answer.setOnClickListener{
             val r = Random()
@@ -78,7 +83,49 @@ class ModernNamiokobana : AppCompatActivity() {
             red_score_kotlin --
             red_score.text = "" + red_score_kotlin
             red_score_in_board.text = "" + red_score_kotlin
+            red_score_in_board_2.text = "" + red_score_kotlin
         }
+        next_round_2.setOnClickListener {
+            round_counter ++
+            score_bar_2.isVisible = false
+            blue_game.isVisible = true
+            timer1()
+            init3()
+        }
+        red_restart.setOnClickListener {
+            red_final_page.isVisible = false
+            team_select_banner.isVisible = true
+            blue_start_button.isVisible = true
+            BackToProfileFromNamiokobana.isVisible = true
+            red_game.isVisible = false
+            makeZero()
+        }
+        blue_restart.setOnClickListener {
+            blue_final_page.isVisible = false
+            team_select_banner.isVisible = true
+            blue_start_button.isVisible = true
+            BackToProfileFromNamiokobana.isVisible = true
+            red_game.isVisible = false
+            makeZero()
+        }
+        draw_restart.setOnClickListener {
+            draw_final_page.isVisible = false
+            team_select_banner.isVisible = true
+            blue_start_button.isVisible = true
+            BackToProfileFromNamiokobana.isVisible = true
+            red_game.isVisible = false
+            makeZero()
+        }
+        BackToProfileFromRedWon.setOnClickListener {
+            startActivity(Intent(this,Profile::class.java))
+        }
+        BackToProfileFromBlueWon.setOnClickListener {
+            startActivity(Intent(this,Profile::class.java))
+        }
+        BackToProfileFromDraw.setOnClickListener {
+            startActivity(Intent(this,Profile::class.java))
+        }
+
     }
     fun init1(){
         if (blue_start_button.isPressed){
@@ -94,8 +141,26 @@ class ModernNamiokobana : AppCompatActivity() {
             red_word.setText(words[n])
         }
     }
+    fun init3(){
+        if(next_round_2.isPressed){
+            val r = Random()
+            val n = r.nextInt(4)
+            blue_word.setText(words[n])
+        }
+    }
+    fun makeZero(){
+        blue_score_kotlin = 0
+        red_score_kotlin = 0
+        round_counter = 1
+        red_score.text = "0"
+        red_score_in_board.text = "0"
+        red_score_in_board_2.text = "0"
+        blue_score.text = "0"
+        blue_score_in_board.text = "0"
+        blue_score_in_board_2.text = "0"
+    }
     private fun timer1() {
-        object : CountDownTimer(30000, 1000) {
+        object : CountDownTimer(2000, 1000) {
 
             override fun onFinish() {
 
@@ -117,7 +182,7 @@ class ModernNamiokobana : AppCompatActivity() {
         }.start()
     }
     private fun timer2() {
-        object : CountDownTimer(30000, 1000) {
+        object : CountDownTimer(2000, 1000) {
 
             override fun onFinish() {
 
@@ -125,6 +190,21 @@ class ModernNamiokobana : AppCompatActivity() {
                     Thread.sleep(100)
                 } catch (e: InterruptedException) { // TODO Auto-generated catch block
                     e.printStackTrace()
+                }
+                if (round_counter == 5){
+                    if(red_score_kotlin > blue_score_kotlin){
+                        score_bar_2.isVisible = false
+                        red_final_page.isVisible = true
+                    }else if (blue_score_kotlin > red_score_kotlin){
+                        score_bar_2.isVisible = false
+                        blue_final_page.isVisible = true
+                    }else{
+                        score_bar_2.isVisible = false
+                        draw_final_page.isVisible = true
+                    }
+                }else{
+                    score_bar_2.isVisible = true
+                    red_game.isVisible = false
                 }
             }
 
